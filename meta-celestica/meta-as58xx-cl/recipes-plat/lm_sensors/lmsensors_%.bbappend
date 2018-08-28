c977b0d6b804cf8f8d19f67a4352de8191a0ae54
zmzhan <mzhan@celestica.com>
2018-08-28 16:49:20 +0800

    add sensors configrue

--- meta-celestica/meta-as58xx-cl/recipes-plat/lm_sensors/lmsensors_%.bbappend
+++ meta-celestica/meta-as58xx-cl/recipes-plat/lm_sensors/lmsensors_%.bbappend
@@ -1,10 +1,12 @@
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 
-SRC_URI += "file://as58xx-cl.conf \
+SRC_URI += "file://fishbone.conf \
+			file://phalanx.conf \
            "
 
 do_install_append() {
     install -d ${D}${sysconfdir}/sensors.d
-    install -m 644 ../as58xx-cl.conf ${D}${sysconfdir}/sensors.d/as58xx-cl.conf
+    install -m 644 ../fishbone.conf ${D}${sysconfdir}/sensors.d/fishbone.conf
+    install -m 644 ../phalanx.conf ${D}${sysconfdir}/sensors.d/phalanx.conf
 }
--- meta-celestica/meta-as58xx-cl/recipes-plat/plat-utils/files/setup_sensors.sh
+++ meta-celestica/meta-as58xx-cl/recipes-plat/plat-utils/files/setup_sensors.sh
@@ -25,18 +25,44 @@ set_value() {
 	echo ${4} > /sys/bus/i2c/devices/i2c-${1}/${1}-00${2}/${3} 2> /dev/null
 }
 
+board_type=$(board_type)
+if [ "$board_type" = "Phalanx" ]; then
+	mv /etc/sensors.d/phalanx.conf /etc/sensors.d/as58xx-cl.conf
+	rm /etc/sensors.d/fishbone.conf
+else
+	mv /etc/sensors.d/fishbone.conf /etc/sensors.d/as58xx-cl.conf
+	rm /etc/sensors.d/phalanx.conf
+fi
+
 #func    bus addr node val
+#IR38060
+set_value 4 43 in0_min 950
+set_value 4 43 in0_max 1050
+set_value 4 43 curr1_min 0
+set_value 4 43 curr1_max 3000
+
+set_value 17 47 in0_min 1164
+set_value 17 47 in0_max 1236
+set_value 17 47 curr1_min 0
+set_value 17 47 curr1_max 1700
+
+#IR38062
+set_value 4 49 in0_min 3135
+set_value 4 49 in0_max 3465
+set_value 4 49 curr1_min 0
+set_value 4 49 curr1_max 10000
+
 #IR3595
-set_value 4 12 in0_min 500
-set_value 4 12 in0_max 2000
-set_value 4 12 curr1_min 0
-set_value 4 12 curr1_max 100000
+set_value 16 12 in0_min 735
+set_value 16 12 in0_max 1020
+set_value 16 12 curr1_min 0
+set_value 16 12 curr1_max 211700
 
 #IR3584
-set_value 4 70 in0_min 500
-set_value 4 70 in0_max 2000
-set_value 4 70 curr1_min 0
-set_value 4 70 curr1_max 100000
+set_value 18 70 in0_min 3135
+set_value 18 70 in0_max 3465
+set_value 18 70 curr1_min 0
+set_value 18 70 curr1_max 31000
 
 #PSU1
 #add it to sensors.config
